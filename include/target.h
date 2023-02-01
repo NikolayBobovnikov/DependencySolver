880c59b73d494959d34c46814978313a12394e42
Nikolay <nbobovnikov@gmail.com>
2023-02-02 00:47:57 +0300

    add utility functions for target

--- include/target.h
+++ include/target.h
@@ -1,7 +1,21 @@
 #include <functional>
+#include <iostream>
+#include <memory>
 
 struct Target {
-  size_t id;
+  Target(size_t _id, std::function<void()> _task) : id(_id), task(_task) {}
 
+  size_t id;
   std::function<void()> task;
+};
+
+struct TargetHelpers {
+  static std::unique_ptr<Target> Create(size_t id, std::function<void()> task) {
+    return std::make_unique<Target>(id, task);
+  }
+
+  static std::unique_ptr<Target> CreateTest(size_t id) {
+    return std::make_unique<Target>(
+        id, [=]() { std::cout << "Building " << id << std::endl; });
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+#include "builder.h"
+#include "target.h"
 
 int main() {
+    auto t = TargetHelpers::CreateTest(0);
+    t->task();
+    
 }
